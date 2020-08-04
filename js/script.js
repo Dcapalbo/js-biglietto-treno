94a0ed76c0935cc701e575559ea250fc6b175340
Domenico Capalbo <capalbodomenico@gmail.com>
2020-08-04 16:14:45 +0200

    inserimento-prompt-età-km
    js-biglietto-treno

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-alert("ciao");
+// chiedere l'eta del passeggero e salvare la variabile
+var età =prompt("Inserisci l'età");
+// chiedere i kilometri che il passeggero desidera percorrere
+var km =prompt("kilometri da percorrere");
+// costo del biglietto in base ai kilometri
+
+//sconto del 20% sé minorenne
+
+// sconto del 40% se over 65
